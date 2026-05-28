e8a10104d7ae2dc56bd8580a27d77ed7a7fdfbc1
james <jameskwan2013@gmail.com>
2026-05-28 01:13:16 -0400

    Updated Tenant.java

--- src/Tenant.java
+++ src/Tenant.java
@@ -1,2 +1,67 @@
 public class Tenant {
+    private String name;    // John Doe
+    private String buildingAddress; // 123 John Street
+    private String apartmentOrStoreNumber;  // 1A
+    private double monthlyRent; // $1,400.00
+    private boolean isCommercial;
+    private double latefee; // monthlyRent * 5%
+
+    public Tenant(String name, String buildingAddress, String apartmentOrStoreNumber, double monthlyRent, boolean isCommercial, double latefee) {
+        this.name = name;
+        this.buildingAddress = buildingAddress;
+        this.apartmentOrStoreNumber = apartmentOrStoreNumber;
+        this.monthlyRent = monthlyRent;
+        this.isCommercial = isCommercial;
+        this.latefee = latefee;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBuildingAddress() {
+        return buildingAddress;
+    }
+
+    public void setBuildingAddress(String buildingAddress) {
+        this.buildingAddress = buildingAddress;
+    }
+
+    public String getApartmentOrStoreNumber() {
+        return apartmentOrStoreNumber;
+    }
+
+    public void setApartmentOrStoreNumber(String apartmentOrStoreNumber) {
+        this.apartmentOrStoreNumber = apartmentOrStoreNumber;
+    }
+
+    public double getMonthlyRent() {
+        return monthlyRent;
+    }
+
+    public void setMonthlyRent(double monthlyRent) {
+        this.monthlyRent = monthlyRent;
+    }
+
+    public boolean isCommercial() {
+        return isCommercial;
+    }
+
+    public void setCommercial(boolean commercial) {
+        isCommercial = commercial;
+    }
+
+    public double getLateFee() {
+        if (this.isCommercial) {
+            return this.monthlyRent * 0.05; // default to 5% late fee
+        } else {
+            return 0.0;
+        }
+    }
+
+
 }
