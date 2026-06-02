39d67c94a763089a48ec0571c918c1e178978b0b
james <jameskwan2013@gmail.com>
2026-06-01 20:20:29 -0400

    Updated Main.java & Tenant.java
    Added getTotalBalance and addPayment in Tenant.java.

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing one month worth of rent on one tenant");
+        System.out.println("Testing two months worth of rent on one tenant");
+        System.out.println();
 
         Tenant John = new Tenant( // name of tenant, address, apt, monthly rent, isCommercial, totalBalance.
-                "John Doe", "230 Grand Street", "1A", 1000, true, 0);
+                "John Doe", "230 Grand Street", "1A", 1000, false, 0.0);
 
         RentPayment january2026 = new RentPayment(  // tenant, month, paymentMethod, amountPaid
                 John, "January", "Check", 100);
@@ -14,23 +15,39 @@ public class Main {
         RentPayment february2026 = new RentPayment(  // tenant, month, paymentMethod, amountPaid
                 John, "February", "Check", 1000);
 
+
+        // === TENANT INFO ===
+        System.out.println("--- Tenant Information ---");
         System.out.println("Tenant name: " + John.getName());
         System.out.println("Address: " + John.getBuildingAddress() + " " + John.getApartmentOrStoreNumber());
         System.out.println("Reisdential/Commercial: " + John.isCommercial());
         System.out.println("Monthly Rent: " + John.getMonthlyRent());
+        System.out.println();
+
+        // === JANUARY DETAILS ===
+        System.out.println("--- Month: " + january2026.getMonth() + " ---");
         System.out.println("Month: " + january2026.getMonth());
-        System.out.println("Month: " + february2026.getMonth());
         System.out.println("Amount paid: " + january2026.getAmountPaid());
-        System.out.println("Amount paid: " + february2026.getAmountPaid());
         System.out.println("Payment method: " + january2026.getPaymentMethod());
-        System.out.println("Payment method: " + february2026.getPaymentMethod());
         System.out.println("Late Fee: " + january2026.getLateFee());
-        System.out.println("Late Fee: " + february2026.getLateFee());
         System.out.println("Monthly Balance: " + january2026.getMonthlyBalance());
+        System.out.println();
+
+        // === FEBRUARY DETAILS ===
+        System.out.println("--- Month: " + february2026.getMonth() + " ---");
+        System.out.println("Month: " + february2026.getMonth());
+        System.out.println("Amount paid: " + february2026.getAmountPaid());
+        System.out.println("Payment method: " + february2026.getPaymentMethod());
+        System.out.println("Late Fee: " + february2026.getLateFee());
         System.out.println("Monthly Balance: " + february2026.getMonthlyBalance());
+        System.out.println();
 
-         System.out.println("Total Balance: " + John.getTotalBalance());
-        // currently set to 0 in Main.java until I add Arraylist that can track multiple months
 
+        John.addPayment(january2026); // testing by adding in 2 months worth of rent and MUST BE BEFORE the get totalBalance
+        John.addPayment(february2026);
+
+        System.out.println();
+        System.out.println("Total Balance: " + John.getTotalBalance());
+        //
     }
 }
--- src/Tenant.java
+++ src/Tenant.java
@@ -9,6 +9,9 @@ public class Tenant {
     private double totalBalance;
 
     private ArrayList<RentPayment> payments;
+    /* payment = single rent payment obj/one month
+       payments = entire rent ledger that stores all records of that tenant
+    */
 
 
     public Tenant(String name, String buildingAddress, String apartmentOrStoreNumber, double monthlyRent, boolean isCommercial, double totalBalance) {
@@ -62,7 +65,18 @@ public class Tenant {
     }
 
     public double getTotalBalance() {
+        // currently set to 0 in Main.java until I add Arraylist that can track multiple months
+        double totalBalance = 0.0;
+
+        for (int i = 0; i < this.payments.size(); i++) {
+            RentPayment payment = this.payments.get(i); // get the individual month
+            totalBalance += payment.getMonthlyBalance();
+        }
         return totalBalance;
     }
 
+
+    public void addPayment(RentPayment payment) {   // Takes one month of payment and adds it to the arraylist payments
+        this.payments.add(payment);
+    }
 }
